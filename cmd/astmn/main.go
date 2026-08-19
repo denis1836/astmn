6d547112295b44ea0e6696064604e2ca6c3ec574
denis1836 <den.pylypen@protonmail.com>
2026-08-19 10:20:00 +0200

    fix: logging and config loading in main

--- cmd/astmn/main.go
+++ cmd/astmn/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"astmn/internal/config"
 	"astmn/internal/db"
 	"astmn/internal/log"
@@ -10,22 +12,25 @@ import (
 var c *config.Config
 
 func main() {
-	_, err := log.InitLogger(c.LogsDir)
+	c, err := config.Load("./astmn.conf.yml")
+	if err != nil {
+		ui.PError("unable to load config" + err.Error())
+		os.Exit(1)
+	}
+
+	logFile, err := log.InitLogger(c.LogsDir)
 	if err != nil {
 		ui.PError("unable init logger: " + err.Error())
-		return
+		os.Exit(1)
 	}
+	defer logFile.Close()
 
 	log.Info("starting...")
-
-	log.Info("loading data from config...")
-	c, err = config.Load("./astmn.conf.yml")
-	if err != nil {
-		log.Errorf("unable to load config: %v", err)
+	log.Info("opening db pool...")
+	if err := db.OpenPool(c.DBPath); err != nil {
+		log.Errorf("error opening db pool: %v", err)
+		os.Exit(1)
 	}
 	log.Info("done!")
 
-	log.Info("opening db pool...")
-	db.OpenPool(c.DBPath)
-	log.Info("done!")
 }
