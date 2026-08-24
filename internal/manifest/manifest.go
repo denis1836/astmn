3f08567fd02861b503fb39355e846940d9c1ecd5
denis1836 <den.pylypen@protonmail.com>
2026-08-24 22:08:53 +0200

    feat(extractor): remove ArchiveType field

--- internal/manifest/manifest.go
+++ internal/manifest/manifest.go
@@ -7,21 +7,20 @@ import (
 )
 
 type Manifest struct {
-	Name string `yaml:"name"`
+	Name    string `yaml:"name"`
 	Version string `yaml:"version"`
-	Date string `yaml:"date"`
-	
-	Author string `yaml:"author"`
-	Description string `yaml:"description"`
-	Contributors [] string `yaml:"contributors"`
+	Date    string `yaml:"date"`
 
-	FileName string `yaml:"file_name"`
+	Author       string   `yaml:"author"`
+	Description  string   `yaml:"description"`
+	Contributors []string `yaml:"contributors"`
+
+	FileName    string `yaml:"file_name"`
 	DownloadURL string `yaml:"download_url"`
-	SHA256 string `yaml:"sha256"`
-	ArchiveType string `yaml:"archive_type"`
+	SHA256      string `yaml:"sha256"`
 	InstallPath string `yaml:"install_path"`
 
-	Contents []string `yaml:"contents"`
+	Contents  []string `yaml:"contents"`
 	DependsOn []string `yaml:"depends_on"`
 	Changelog []string `yaml:"changelog"`
 }
@@ -36,6 +35,6 @@ func Load(path string) (*Manifest, error) {
 	if err := yml.Unmarshal(data, &m); err != nil {
 		return nil, err
 	}
-	 
+
 	return &m, nil
 }
